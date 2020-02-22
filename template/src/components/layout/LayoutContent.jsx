9cf4bf7e99b0261bed25c51f7d8cc9109cf5cdfc
赵凯 <820641374@qq.com>
2020-02-22 23:52:38 +0800

    udpate

--- template/src/components/layout/LayoutContent.jsx
+++ template/src/components/layout/LayoutContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Index from '../../pages/index/index';
+import Index from '../../pages/index/Index';
 
 function LayoutContent(props) {
     return (
---
+++ template/src/store/index.js
@@ -0,0 +1,4 @@
+
+export default {
+    AppStore: require('./modules/appStore').default
+}
