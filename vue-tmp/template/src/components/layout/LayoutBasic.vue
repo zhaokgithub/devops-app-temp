a8379095fa7d0cc66aaf73bee39da30fb55e84ad
zhaokai <820641374@qq.com>
2020-03-21 22:41:47 +0800

    udpate

--- vue-tmp/template/src/components/layout/LayoutBasic.vue
+++ vue-tmp/template/src/components/layout/LayoutBasic.vue
@@ -6,13 +6,14 @@
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b"
+        style="height:100vh;text-align:left;"
       >
-        <el-menu-item index="2-4-1">
+        <el-menu-item index="index">
            <template slot="title">
             <i class="el-icon-s-home"></i><span>首页</span>
           </template>
         </el-menu-item>
-         <el-menu-item index="2-4-1">
+         <el-menu-item index="personal">
            <template slot="title">
             <i class="el-icon-s-home"></i><span>个人中心</span>
           </template>
@@ -22,7 +23,7 @@
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
+      <el-header style="text-align: right; font-size: 12px;background:#fff;height:40px;line-height:40px;">
         <el-dropdown>
           <i class="el-icon-setting" style="margin-right: 15px"></i>
           <el-dropdown-menu slot="dropdown">
@@ -31,15 +32,10 @@
             <el-dropdown-item>删除</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span>王小虎</span>
+        <span>{{userInfo.name}}</span>
       </el-header>
-
-      <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-          <el-table-column prop="address" label="地址"></el-table-column>
-        </el-table>
+      <el-main style="backgroun:#F5F7F9;">
+        <router-view></router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -49,10 +45,19 @@
 export default {
   components: {},
   data() {
-    return {};
+    return {
+      userInfo:{name:"Demo"}
+    };
   },
   methods: {},
-  created() {},
+  created() {
+    console.log(this.$store.state)
+  },
   mounted() {}
 };
 </script>
+<style>
+.el-main{
+  background: #F5F7F9;
+}
+</style>
