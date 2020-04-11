5a62e3d5b2582671d68d055ea11fa4016a340d4e
zhaokai <820641374@qq.com>
2020-04-11 14:53:45 +0800

    update

--- react-tmp/template/src/store/modules/appStore.js
+++ react-tmp/template/src/store/modules/appStore.js
@@ -2,7 +2,11 @@ import {observable,action} from 'mobx'
 
 class AppStore{
     @observable userInfo = null
+    //记录当前路由
     @observable pathname = ''
+    //列表查询参数
+    @observable searchParams={}
+    @observable pagination={page:1,size:10}
 
     @action
     setUserInfo = function(userInfo){
@@ -11,6 +15,14 @@ class AppStore{
     @action
     setPathName = function(pathname){
         this.pathname = pathname
+    }
+    @action
+    setPagination = function(pagination){
+        this.pagination = Object.assign({},this.pagination,pagination)
+    }
+    @action
+    setPagination =function(params) {
+        this.searchParams = Object.assign({},this.searchParams,params)
     }
 }
 
