3873b84c93ca53fe8255d6ac9883a4edecfb56db
zhaokai <zhaokai@appledeMacBook-Air.local>
2020-08-07 01:03:50 +0800

    update

--- react-tmp/template/src/pages/index/index.jsx
+++ react-tmp/template/src/pages/index/index.jsx
@@ -1,100 +1,27 @@
 import React, { Component } from 'react';
 import Api from '../../service/api';
 import { Row, Col, Button, Icon, Tabs } from 'antd';
-import Sortable from "sortablejs";
-import './index.less'
+import {inject,observer} from 'mobx-react'
+import './index.less';
 
 const { TabPane } = Tabs;
 
+@inject('AppStore')
+@observer
 class Index extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            list: [],
-            selectedKey: ''
         }
 
     }
     componentDidMount() {
-        this.initTmpLayout()
-        this.initTmpContainer()
     }
-    initTmpLayout() {
-        let self = this
-        let leftLayout = document.querySelector(".devops-form-layout")
-        Sortable.create(leftLayout, {
-            group: {
-                name: "leftLayout",
-                pull: 'clone',
-                put: false,
-            },
-            onEnd: function (/**Event*/evt) {
-                let type = evt.item.dataset.type
-                let list = self.state.list
-                let key = new Date()
-                let obj = { type: type, key: key.valueOf() }
-                list.push(obj)
-                self.setState({ list, selectedKey: key.valueOf() })
-            },
+    
 
-        })
-    }
-    initTmpContainer() {
-        let centerContainer = document.querySelector(".devops-form-container")
-        Sortable.create(centerContainer, {
-            group: {
-                name: 'leftLayout',
-                put: false
-            },
-            animation: 150,
-        })
-    }
     render() {
-        let { list } = this.state
-        console.log(list)
-        let basicWidget = [
-            { icon: "file-text", txt: "单行文本", type: 1 },
-            { icon: "file-text", txt: "多行文本", type: 2 },
-            { icon: "clock-circle", txt: "时间选择器", type: 3 },
-            { icon: "number", txt: "计数器", type: 4 },
-            { icon: "select", txt: "选择器", type: 5 },
-        ]
-        return (
-            <Row className="devops-layout-index">
-                <Col span={4} className="devops-block">
-                    <p>基础控件</p>
-                    <div className="devops-form-layout" style={{ cursor: 'pointer' }}>
-                        {
-                            basicWidget.map(item => {
-                                return <div className="form-widget" data-type={item.type}>
-                                    <Icon type={item.icon} /><span style={{ marginLeft: 10 }}>{item.txt}</span>
-                                </div>
-                            })
-                        }
-                    </div>
-                </Col>
-                <Col span={16} className="devops-block">
-                    <div className="devops-form-container">
-                        {
-                            list.map(item => {
-                                return <div
-                                    className={item.key == this.state.selectedKey ? 'form-item form-item-selected' : 'form-item'}
-                                    key={item.key}>{item.type}</div>
-                            })
-                        }
-                    </div>
-                </Col>
-                <Col span={4}>
-                    <div className="devops-form-setting">
-                        <Tabs defaultActiveKey="1">
-                            <TabPane tab="字段属性" key="1">字段属性</TabPane>
-                            <TabPane tab="表单属性" key="3">表单属性</TabPane>
-                        </Tabs>
-                    </div>
-
-                </Col>
-            </Row>
-        )
+        
+        return (<div>test</div> )
     }
 }
 
--- react-tmp/template/src/store/modules/appStore.js
+++ react-tmp/template/src/store/modules/appStore.js
@@ -1,11 +1,11 @@
 import {observable,action} from 'mobx'
 
 class AppStore{
-    @observable userInfo = null
+    @observable userInfo = {name:'name'}
     //记录当前路由
     @observable pathname = ''
     //列表查询参数
-    @observable searchParams={}
+    @observable searchParams={name:'name2'}
     @observable pagination={page:1,size:10}
 
     @action
@@ -16,14 +16,11 @@ class AppStore{
     setPathName = function(pathname){
         this.pathname = pathname
     }
-    @action
-    setPagination = function(pagination){
-        this.pagination = Object.assign({},this.pagination,pagination)
-    }
+ 
     @action
     setPagination =function(params) {
         this.searchParams = Object.assign({},this.searchParams,params)
     }
 }
 
-export default AppStore
+export default new AppStore
